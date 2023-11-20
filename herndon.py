cb1f0937bda475d5961d0e2d91de495425d99fb4
boppabopp <148220992+boppabopp@users.noreply.github.com>
2023-11-20 06:23:13 -0500

    Update herndon.py

--- herndon.py
+++ herndon.py
@@ -10,22 +10,6 @@ class Herndon(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.midbottom = (screen.get_width() // 2 + 80, 660)
 
-    def draw_triangle(self):
-        # Create the blue triangle surface
-        triangle_width = 50
-        triangle_height = 100
-        triangle_color = (0, 0, 255)  # Blue color
-        triangle = pygame.Surface((triangle_width, triangle_height), pygame.SRCALPHA)
-        pygame.draw.polygon(triangle, triangle_color,
-                            [(0, triangle_height), (triangle_width // 2, 0), (triangle_width, triangle_height)])
-
-        # Calculate the position to draw the triangle above the top of the tower
-        triangle_x = (self.image.get_width() - triangle_width) // 2
-        triangle_y = self.rect.top - triangle_height
-
-        # Blit the blue triangle onto the tower surface
-        self.image.blit(triangle, (triangle_x, triangle_y))
-
     def draw(self, screen):
         # Create a temporary surface to hold the tower and triangle
         temp_surface = pygame.Surface((300, 500), pygame.SRCALPHA)
